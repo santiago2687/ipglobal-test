060eacd0e1509817bb79d03296a44c2748640668
Santiago Bermudez <santiago2687@gmail.com>
2020-10-10 04:14:09 -0300

    add image thumbnail when the movie doens't have image

--- public/js/movies.js
+++ public/js/movies.js
@@ -1,45 +1,48 @@
-jQuery(function() {
-    $('#searchForm').on('submit', (e) => {
-        let movieName = $('#searchText').val();
-        getMovies(movieName);
-        e.preventDefault();
-    });
+jQuery(function () {
+  $('#searchForm').on('submit', (e) => {
+    let movieName = $('#searchText').val();
+    getMovies(movieName);
+    e.preventDefault();
+  });
 })
 
 function getMovies(movieName) {
-    axios.get('app/api/getMovies.php?movieName=' + movieName)
-        .then((response) => {
-            let movies = JSON.parse(response.data).Search;
-            let output = '';
-            $.each(movies, (index, movie) => {
-                output += `
-            <div class="col-md-3">
+  axios.get('app/api/getMovies.php?movieName=' + movieName)
+    .then((response) => {
+      let movies = JSON.parse(response.data).Search;
+      let output = '';
+      $.each(movies, (index, movie) => {
+        console.log(movie);
+        output += `
+            <div class="col-md-3 movie-card">
               <div class="well text-center">
-                <img src="${movie.Poster}">
+                <img class="${ movie.Poster == 'N/A' ? 'img-thumbnail img-fluid' : '' }" 
+                      src="${ movie.Poster == 'N/A' ? './public/img/default-thumbnail.jpg' : movie.Poster }">
                 <h5>${movie.Title}</h5>
                 <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
               </div>
             </div>
           `;
-            });
+      });
 
-            $('#movies').html(output);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+      $('#movies').html(output);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
 function getMovie() {
-    let movieId = sessionStorage.getItem('movieId');
+  let movieId = sessionStorage.getItem('movieId');
 
-    axios.get('app/api/getInfoMovie.php?movieId=' + movieId)
-        .then((response) => {
-            let movie = JSON.parse(response.data);
-            let output = `
+  axios.get('app/api/getInfoMovie.php?movieId=' + movieId)
+    .then((response) => {
+      let movie = JSON.parse(response.data);
+      let output = `
           <div class="row">
             <div class="col-md-4">
-              <img src="${movie.Poster}" class="thumbnail">
+              <img class="${ movie.Poster == 'N/A' ? 'img-thumbnail img-fluid' : '' }" 
+                      src="${ movie.Poster == 'N/A' ? './public/img/default-thumbnail.jpg' : movie.Poster }">
             </div>
             <div class="col-md-8">
               <h2>${movie.Title}</h2>
@@ -65,15 +68,15 @@ function getMovie() {
           </div>
         `;
 
-            $('#movie').html(output);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+      $('#movie').html(output);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
 function movieSelected(id) {
-    sessionStorage.setItem('movieId', id);
-    window.location = 'movie_info.php';
-    return false;
+  sessionStorage.setItem('movieId', id);
+  window.location = 'movie_info.php';
+  return false;
 }
